efde70918e072ed6081f8beac193293d48059630
Julien Gouesse <gouessej@orange.fr>
2015-08-28 18:39:50 +0200

    Updates JOGL (2.3.2 RC 20150812), moves the image format detection from Ardor3D to JOGL

--- ardor3d-jogl/src/main/java/com/ardor3d/image/util/jogl/JoglTgaImageLoader.java
+++ ardor3d-jogl/src/main/java/com/ardor3d/image/util/jogl/JoglTgaImageLoader.java
@@ -3,7 +3,7 @@
  *
  * This file is part of Ardor3D.
  *
- * Ardor3D is free software: you can redistribute it and/or modify it 
+ * Ardor3D is free software: you can redistribute it and/or modify it
  * under the terms of its license which may be found in the accompanying
  * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
  */
@@ -13,7 +13,9 @@ package com.ardor3d.image.util.jogl;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.ardor3d.image.Image;
 import com.ardor3d.image.util.ImageLoaderUtil;
+import com.jogamp.opengl.util.texture.TextureData;
 import com.jogamp.opengl.util.texture.TextureIO;
 
 /**
@@ -25,8 +27,13 @@ public class JoglTgaImageLoader extends JoglImageLoader {
     private static final String[] _supportedFormats = new String[] { "." + TextureIO.TGA.toUpperCase() };
 
     @Override
-    protected String getFileSuffix(final InputStream is) throws IOException {
-        return TextureIO.TGA;
+    public Image load(final InputStream is, final boolean verticalFlipNeeded) throws IOException {
+        final TextureData textureData = TextureIO.newTextureData(_capsUtil.getProfile(), is,
+                isMipmapsProductionEnabled(), TextureIO.TGA);
+        if (textureData == null) {
+            return null;
+        }
+        return makeArdor3dImage(textureData, textureData.getMustFlipVertically() == verticalFlipNeeded);
     }
 
     public static String[] getSupportedFormats() {
