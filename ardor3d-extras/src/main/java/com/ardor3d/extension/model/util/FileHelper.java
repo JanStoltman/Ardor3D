598de90095319402854929ca3133709c0673ce95
Julien Gouesse <gouessej@orange.fr>
2016-07-07 20:57:28 +0200

    Adds a STL importer into ardor3d-extras

--- ardor3d-extras/src/main/java/com/ardor3d/extension/model/util/FileHelper.java
+++ ardor3d-extras/src/main/java/com/ardor3d/extension/model/util/FileHelper.java
@@ -13,12 +13,15 @@ package com.ardor3d.extension.model.util;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.StandardCharsets;
+
+import com.ardor3d.util.resource.ResourceSource;
 
 public class FileHelper {
 
@@ -57,6 +60,32 @@ public class FileHelper {
     public boolean isFilePureAscii(final String filename) {
         try (final FileInputStream fstream = new FileInputStream(filename);
                 final DataInputStream in = new DataInputStream(fstream);
+                final BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String strLine;
+            // read file a line at a time
+            while ((strLine = br.readLine()) != null) {
+                final boolean isAscii = isStringPureAscii(strLine);
+                if (!isAscii) {
+                    return false;
+                }
+            }
+        } catch (final Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Tests whether or not the file with the specified filename is pure ASCII. The method used is to read the file a
+     * line at a time and test if each line is ASCII.
+     *
+     * @param resource
+     *            the name of the resource to find.
+     * @return Logical-true if pure ASCII, else logical-false.
+     */
+    public boolean isFilePureAscii(final ResourceSource resource) {
+        try (final InputStream inputStream = resource.openStream();
+                final DataInputStream in = new DataInputStream(inputStream);
                 final BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
             String strLine;
             // read file a line at a time
